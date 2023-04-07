169662e64110c75a466152273b98f8eeac326ea3
Rodrigo Rocha <rodrigorgs@gmail.com>
2023-04-06 23:32:25 -0300

    Close popup after submit

--- popup.js
+++ popup.js
@@ -1,11 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {  
   document.getElementById('action').addEventListener('click', async () => {
     const userText = document.getElementById("user-text").value;
-    
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-    
     chrome.runtime.sendMessage({ type: 'searchProcesso', processo: userText });
-
-
+    window.close();
   });
 });
